9cfefb411805920bc950bc185d521ff2106739f6
aslamcontact <aslam.mycontact@gmail.com>
2023-12-12 07:34:56 +0530

    add propertied in ventors personal details

---
+++ ventors/src/main/java/com/aslam/mycontact/ventors/daolayer/ventordetails/VentorPersonalDetails.java
@@ -0,0 +1,50 @@
+package com.aslam.mycontact.ventors.daolayer.ventordetails;
+
+public class VentorPersonalDetails {
+
+    private String country;
+    private String state;
+    private String district;
+    private String city;
+    private Integer pinCode;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(Integer pinCode) {
+        this.pinCode = pinCode;
+    }
+}
