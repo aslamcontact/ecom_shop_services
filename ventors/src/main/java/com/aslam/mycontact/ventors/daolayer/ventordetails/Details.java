3e10b4b8f62a2a78d587bc22007620490bf6f1c4
aslamcontact <aslam.mycontact@gmail.com>
2023-12-12 07:36:20 +0530

    add propertied in details class that used to describe about the ventor to their client

---
+++ ventors/src/main/java/com/aslam/mycontact/ventors/daolayer/ventordetails/Details.java
@@ -0,0 +1,50 @@
+package com.aslam.mycontact.ventors.daolayer.ventordetails;
+
+public class Details {
+
+    private String brandDescription;
+    private String contactNumber;
+    private String emailId;
+    private String instagramId;
+    private String whatsAppNumber;
+
+    public String getBrandDescription() {
+        return brandDescription;
+    }
+
+    public void setBrandDescription(String brandDescription) {
+        this.brandDescription = brandDescription;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getInstagramId() {
+        return instagramId;
+    }
+
+    public void setInstagramId(String instagramId) {
+        this.instagramId = instagramId;
+    }
+
+    public String getWhatsAppNumber() {
+        return whatsAppNumber;
+    }
+
+    public void setWhatsAppNumber(String whatsAppNumber) {
+        this.whatsAppNumber = whatsAppNumber;
+    }
+}
