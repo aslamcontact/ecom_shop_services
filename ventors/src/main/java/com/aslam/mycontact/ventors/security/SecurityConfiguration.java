69a0716267332a79b3c19e38cbbd519c235d753e
aslamcontact <aslam.mycontact@gmail.com>
2023-12-15 01:41:20 +0530

    security filter chain

--- ventors/src/main/java/com/aslam/mycontact/ventors/security/SecurityConfiguration.java
+++ ventors/src/main/java/com/aslam/mycontact/ventors/security/SecurityConfiguration.java
@@ -2,9 +2,15 @@ package com.aslam.mycontact.ventors.security;
 
 import com.aslam.mycontact.ventors.security.jwt.JwtAuthFilter;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -17,4 +23,22 @@ public class SecurityConfiguration {
         this.jwtAuthFilter = jwtAuthFilter;
         this.authenticationProvider = authenticationProvider;
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+
+        http.authorizeHttpRequests((auth)->
+                        auth.requestMatchers("/api/ventor/login/**").permitAll()
+                                .requestMatchers("/api/ventor/app/**").authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((session)->session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+
 }
