d78f2a05d8a9bfe3aa83e405f3ff307f9889a1ca
aslamcontact <aslam.mycontact@gmail.com>
2023-12-14 20:50:13 +0530

    add secret key(minimun 256 bytes) and implements the getsinginkey method that decode secret key to hmacshakey and generate token method

---
+++ ventors/src/main/java/com/aslam/mycontact/ventors/security/jwt/JwtService.java
@@ -0,0 +1,44 @@
+package com.aslam.mycontact.ventors.security.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    private  static final String SECRET_KEY="41c173246c1632ee0becd7556cb66e3d024118888e963fa1b68a19e5eff61e51";
+    private Key getSignInKey()
+    {
+        byte[] byteKey= Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(byteKey);
+
+    }
+
+    public String generateTokens(UserDetails userDetails)
+    {
+        return generateToken(new HashMap<>(),userDetails);
+    }
+
+    public String generateToken(
+            Map<String,Object> extraClaims,
+            UserDetails userDetails
+    )
+    {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
