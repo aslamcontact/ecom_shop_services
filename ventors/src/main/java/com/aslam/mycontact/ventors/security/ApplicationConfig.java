39833ecffeac04cf3c883af81aa033ebc69da06a
aslamcontact <aslam.mycontact@gmail.com>
2023-12-15 01:29:02 +0530

    add authenticationProvider,AuthenticationManger and password encoder bean

--- ventors/src/main/java/com/aslam/mycontact/ventors/security/ApplicationConfig.java
+++ ventors/src/main/java/com/aslam/mycontact/ventors/security/ApplicationConfig.java
@@ -3,8 +3,14 @@ package com.aslam.mycontact.ventors.security;
 import com.aslam.mycontact.ventors.daolayer.VentorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class ApplicationConfig {
     @Autowired
@@ -18,5 +24,25 @@ public class ApplicationConfig {
         )
                 ;
     }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider()
+    {
+        DaoAuthenticationProvider authProvider= new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
