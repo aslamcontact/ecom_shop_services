d3893c4a39619041b31fc827ccec370fc0e95196
aslamcontact <aslam.mycontact@gmail.com>
2023-12-15 01:18:26 +0530

    add method get ventor by email

--- ventors/src/main/java/com/aslam/mycontact/ventors/daolayer/VentorRepository.java
+++ ventors/src/main/java/com/aslam/mycontact/ventors/daolayer/VentorRepository.java
@@ -3,6 +3,10 @@ package com.aslam.mycontact.ventors.daolayer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VentorRepository extends JpaRepository<Ventor,Integer> {
+
+    public Optional<Ventor> findByemail(String email);
 }
