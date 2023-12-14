ca0506ce908ad695e15ec0ca1d3228d28751ae6a
aslamcontact <aslam.mycontact@gmail.com>
2023-12-15 01:32:48 +0530

    add conguration annotation which i forgot

--- ventors/src/main/java/com/aslam/mycontact/ventors/security/ApplicationConfig.java
+++ ventors/src/main/java/com/aslam/mycontact/ventors/security/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.aslam.mycontact.ventors.security;
 import com.aslam.mycontact.ventors.daolayer.VentorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class ApplicationConfig {
     @Autowired
     private VentorRepository  ventorRepository;
